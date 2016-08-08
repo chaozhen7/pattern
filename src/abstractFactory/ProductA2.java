7440a718be2c43faf5a0858104d6a59b509b2d97
Chaos_sina <Chaos@sina.com>
2016-08-08 19:15:57 +0800

    抽象工厂模式

---
+++ src/abstractFactory/ProductA2.java
@@ -0,0 +1,11 @@
+package abstractFactory;
+/**
+ * @author sina Email:chaozhen7@163.com
+ * @date 2016年8月2日 下午8:04:59
+ * @version 1.0
+ */
+public class ProductA2 extends AbstractProductA {
+	public ProductA2(){
+		System.out.println("A2 init");
+	}
+}
---
+++ src/abstractFactory/ProductB2.java
@@ -0,0 +1,11 @@
+package abstractFactory;
+/**
+ * @author sina Email:chaozhen7@163.com
+ * @date 2016年8月2日 下午8:04:59
+ * @version 1.0
+ */
+public class ProductB2 extends AbstractProductB {
+	public ProductB2(){
+		System.out.println("B2 init");
+	}
+}
