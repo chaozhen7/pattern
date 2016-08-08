272acc14756ae03403b9c052af4a4a4cbe10dfa4
Chaos_sina <Chaos@sina.com>
2016-08-08 19:17:51 +0800

    外观模式

---
+++ src/facade/Client.java
@@ -0,0 +1,15 @@
+package facade;
+/**
+ * @author sina Email:chaozhen7@163.com
+ * @date 2016年8月3日 上午11:23:53
+ * @version 1.0
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Facade facade = new Facade();
+		facade.method();
+	}
+
+}
